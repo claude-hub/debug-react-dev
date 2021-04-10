8c5931f7269e15b62c4ee86a864462709cae7375
zhangyunpeng <zhangyunpeng@sensorsdata.cn>
2021-04-10 23:55:34 +0800

    feat: add page

--- src/index.js
+++ src/index.js
@@ -1,32 +1,34 @@
 // import React from "react";
 // import ReactDOM from "react-dom";
 import React from "./react/react";
-import ReactDom from "./react/react-dom";
+import ReactDOM from "./react/react-dom";
 import "./index.css";
-// import App from "./App";
-// import Concurrent from "./pages/concurrent";
-// import Level from "./pages/level";
 
 const rootEl = document.querySelector("#root");
 
-ReactDom.render(
-  React.createElement(
-    "div",
-    null,
-    React.createElement("strong", null, "Hello"),
-    React.createElement("span", null, "World!")
-  ),
-  rootEl
-);
+const updateValue = (e) => {
+  reRender(e.target.value);
+};
 
-// ReactDOM.render(<App />, rootEl);
+/** jsx
+<div>
+  <input onInput={updateValue} value={value} />
+  <h2>Hello {value}</h2>
+</div>
+*/
+const reRender = (value) => {
+  ReactDOM.render(
+    React.createElement(
+      "div",
+      null,
+      React.createElement("input", {
+        onInput: updateValue,
+        value: value,
+      }),
+      React.createElement("h2", null, "Hello ", value)
+    ),
+    rootEl
+  );
+};
 
-// ReactDOM.unstable_createRoot(rootEl).render(
-//   <Concurrent />
-// );
-
-// 并发模式从 0 - 2 - 3
-// ReactDOM.unstable_createRoot(rootEl).render(<Level />);
-
-// 同步模式从 0 - 1 - 3
-// ReactDOM.render(<Level />, rootEl);
+reRender("World");
